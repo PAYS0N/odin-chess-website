f122ba7973c12da68b87d4bb58c359217ce36a01
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-12-22 17:49:37 -0600

    Add parsing for move inputs

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -16,12 +16,50 @@ module OdinChess
 
     def apply_move(move)
       @game_state += 1
+      puts "move not applied"
       @game_ended = true if @game_state > 9
     end
 
     def parse(move)
-      puts "unparsed move: #{move}"
-      move
+      move = move.chars
+      return parse_castle(move) if move.include?("0")
+
+      if move.include?("x")
+        return parse_piece_capture(move) if move.any? { |char| char.match?(/[A-Z]/) }
+
+        parse_pawn_capture(move)
+      else
+        return parse_piece_move(move) if move.any? { |char| char.match?(/[A-Z]/) }
+
+        parse_pawn_move(move)
+      end
+    rescue NoMethodError => e
+      raise unless e.message.include?("undefined method `ord'")
+
+      ["Invalid"]
+    end
+
+    def parse_castle(move)
+      return ["Castle"] if move == %w[0 - 0]
+      return ["Long castle"] if move == %w[0 - 0 - 0]
+
+      ["Invalid"]
+    end
+
+    def parse_piece_capture(move)
+      [move[0], move[1].ord - 97, move[2].to_i - 1, move[4].ord - 97, move[5].to_i - 1]
+    end
+
+    def parse_pawn_capture(move)
+      ["P", move[0].ord - 97, move[1].to_i - 1, move[3].ord - 97, move[4].to_i - 1]
+    end
+
+    def parse_piece_move(move)
+      [move[0], move[1].ord - 97, move[2].to_i - 1, move[3].ord - 97, move[4].to_i - 1]
+    end
+
+    def parse_pawn_move(move)
+      ["P", move[0].ord - 97, move[1].to_i - 1, move[2].ord - 97, move[3].to_i - 1]
     end
 
     def valid?(move)
---
+++ spec/game_board_spec.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+require_relative("../lib/game_board")
+
+describe OdinChess::GameBoard do
+  describe "#parse" do
+    subject(:board_parse) { described_class.new(0, 0, 0) }
+
+    context "when given \"0-0\"" do
+      it "calls parse_castle" do
+        expect(board_parse).to receive(:parse_castle)
+        board_parse.parse("0-0")
+      end
+      it "returns [\"Castle\"]" do
+        parse = board_parse.parse("0-0")
+        expect(parse).to eq(["Castle"])
+      end
+    end
+
+    context "when given \"0-0-0\"" do
+      it "calls parse_castle" do
+        expect(board_parse).to receive(:parse_castle)
+        board_parse.parse("0-0-0")
+      end
+      it "returns [\"Long castle\"]" do
+        parse = board_parse.parse("0-0-0")
+        expect(parse).to eq(["Long castle"])
+      end
+    end
+
+    context "when given Ra8a7" do
+      it "calls parse_piece_move" do
+        expect(board_parse).to receive(:parse_piece_move)
+        board_parse.parse("Ra8a7")
+      end
+      it "returns [R, 0, 7, 0, 6]" do
+        parse = board_parse.parse("Ra8a7")
+        expect(parse).to eq(["R", 0, 7, 0, 6])
+      end
+    end
+
+    context "when given e6e7" do
+      it "calls parse_pawn_move" do
+        expect(board_parse).to receive(:parse_pawn_move)
+        board_parse.parse("e6e7")
+      end
+      it "returns [P, 4, 5, 4, 6]" do
+        parse = board_parse.parse("e6e7")
+        expect(parse).to eq(["P", 4, 5, 4, 6])
+      end
+    end
+
+    context "when given Ba8xa7" do
+      it "calls parse_piece_capture" do
+        expect(board_parse).to receive(:parse_piece_capture)
+        board_parse.parse("Ba8xa7")
+      end
+      it "returns [B, 0, 7, 0, 6]" do
+        parse = board_parse.parse("Ba8xa7")
+        expect(parse).to eq(["B", 0, 7, 0, 6])
+      end
+    end
+
+    context "when given e4xd5" do
+      it "calls parse_pawn_capture" do
+        expect(board_parse).to receive(:parse_pawn_capture)
+        board_parse.parse("e4xd5")
+      end
+      it "returns [P, 4, 3, 3, 4]" do
+        parse = board_parse.parse("e4xd5")
+        expect(parse).to eq(["P", 4, 3, 3, 4])
+      end
+    end
+
+    context "when given invalid moves" do
+      ["11111", "Ra88a7", "xxxxXx", "", "Rj4of", "j1xj2", "PE3E4", "00-00", "e4e5Px", "e4w5"].each do |input|
+        it "returns non-valid move for #{input.inspect}" do
+          parse = board_parse.parse(input)
+          expect(parse.shift == "Invalid" || parse.any? { |char| !char.between?(0, 7) }).to be_truthy
+        end
+      end
+    end
+  end
+end
