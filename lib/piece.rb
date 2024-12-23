b2fd0ebfc3446d27e1d32c541bc05796a3c4b506
Payson Eberz <63822744+PAYS0N@users.noreply.github.com>
2024-12-23 15:53:12 -0600

    Add dark color to black pieces

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "colorize"
+
 # module to store chess game
 module OdinChess
   # class that stores piece data
@@ -21,10 +23,23 @@ module OdinChess
     end
 
     def piece_to_s
+      return white_piece_char if @color == "w"
+
+      black_piece_char
+    end
+
+    def white_piece_char
       return " N " if @type == "knight"
       return "   " if @type == "empty"
 
       " #{@type[0].upcase} "
+    end
+
+    def black_piece_char
+      return " N ".colorize(:light_black) if @type == "knight"
+      return "   ".colorize(:light_black) if @type == "empty"
+
+      " #{@type[0].upcase} ".colorize(:light_black)
     end
   end
 end
